9e5ef062ae1af2b2541a60c2931f424cf6686b0b
dbojaxhiu1 <diar.bojaxhi@gmail.com>
2020-10-31 23:13:57 +0100

    add set date option

--- app/src/main/java/com/example/medregister/AddScheduledPillActivity.java
+++ app/src/main/java/com/example/medregister/AddScheduledPillActivity.java
@@ -2,7 +2,29 @@ package com.example.medregister;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AddScheduledPillActivity extends AppCompatActivity {
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import java.util.Calendar;
+
+public class AddScheduledPillActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     public static final String TAG = "AddScheduledPillActivity";
+
+
+    public void setDate() {
+        Calendar now = Calendar.getInstance();
+        DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(
+                this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.setMinDate(Calendar.getInstance());
+        datePickerDialog.show(getFragmentManager(), "DatePickerDialog");
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+
+    }
 }
